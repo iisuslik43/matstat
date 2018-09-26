74da6a7ae67545d0610a80992c3cdd01a0f3af30
iisuslik <al.s.fedotov1998@yandex.ru>
2018-09-26 11:05:01 +0300

    description

--- hw3/hw3.py
+++ hw3/hw3.py
@@ -1,11 +1,16 @@
 import numpy as np
 import math
 import matplotlib.pyplot as plt
-from scipy.stats import moment
 
-distribution = int(input("Type 1 for uniform or 2 for exponential distribution:"))
+def moment(list, k):
+	res = 0.0
+	for el in list:
+		res += pow(el, k)
+	return res / len(list)
 
-theta = float(input("Type theta:"))
+distribution = int(input("Type 1 for uniform distribution or 2 for exponential distribution: "))
+
+theta = float(input("Type theta: "))
 
 graphicPoints = []
 
@@ -16,18 +21,12 @@ for k in range(1, 50):
 		if (distribution == 1):
 			sample = np.random.uniform(0, theta, 50)
 			m_k = moment(sample, k)
-			if m_k < 0.0000001:
-				continue
 			theta_1 = pow(m_k * (k + 1), 1 / k)
 		elif (distribution == 2):
 			sample = np.random.exponential(theta, 50)
 			m_k = moment(sample, k)
 			theta_1 = pow(m_k / math.factorial(k), 1 / k)
-		deltas.append(abs(theta - theta_1))
-	if (len(deltas) != 0):
-		graphicPoints.append(np.mean(deltas))
-if (distribution == 2):
-	plt.plot([i for i in range(1, 50)], graphicPoints)
-else:
-	plt.plot([i for i in range(2, 50)], graphicPoints)
+		deltas.append(pow(theta - theta_1, 2))
+	graphicPoints.append(np.mean(deltas))
+plt.plot([i for i in range(1, 50)], graphicPoints)
 plt.show()
